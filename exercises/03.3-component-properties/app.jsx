d0deaed6762f6a49d5637ee85585317d9b8bf786
jeffreyally <jeffreyally@gmail.com>
2022-01-28 02:21:25 +0000

    3.3 component properties exercise

--- exercises/03.3-component-properties/app.jsx
+++ exercises/03.3-component-properties/app.jsx
@@ -1,26 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropType from "prop-types";
+//description = "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+//imageURL = "https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+//buttonURL = "https://en.wikipedia.org/wiki/Bob_Dylan"
+//buttonLabel = "Go to wikipedia"
 
-const BootstrapCard = props => {
+const BootstrapCard = (props) => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
+			<img className="card-img-top" src={props.imageURL} alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
-				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
-					Go to wikipedia
+				<p className="card-text">{props.description}</p>
+				<a href={props.buttonURL} className="btn btn-primary">
+					{props.buttonLabel}
 				</a>
 			</div>
 		</div>
 	);
 };
 BootstrapCard.propTypes = {
-	title: PropType.string
-		// 2) add here the new properties into the proptypes object
+	description: PropType.string,
+	imageURL: PropType.string,
+	buttonURL: PropType.string,
+	title: PropType.string,
+	buttonLabel: PropType.string,
 };
 
-
-// 3) Use ReactDOM to add the component into then DOM element #myDiv
+ReactDOM.render(
+	<BootstrapCard
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+		imageURL="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+		buttonURL="https://en.wikipedia.org/wiki/Bob_Dylan"
+		title="Bob Dylan"
+		buttonLabel="Go to wikipedia"
+	/>,
+	document.querySelector("#myDiv")
+);
