2de59fbb50cb41a2e49813a427833fdbfdca6fab
jeffreyally <jeffreyally@gmail.com>
2022-01-31 23:22:44 +0000

    test

--- .learn/resets/01.2-hello-jsx/app.jsx
+++ .learn/resets/01.2-hello-jsx/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // if we have a variable
-let age = "122222";
+let age = "12";
 let name = "John";
 
 // we can use it in our html like this
